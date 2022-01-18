b21f9ed899ddc0e669cbb6a285efb15f76f30486
halysson <halyssonfreitas@gmail.com>
2022-01-18 17:44:21 -0300

    #update to the Global Scoped Pipes in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    }],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
