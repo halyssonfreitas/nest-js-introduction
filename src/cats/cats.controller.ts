e1581042acdf288a8aa700b4b57f6b9ba026e8ab
halysson <halyssonfreitas@gmail.com>
2022-01-19 09:39:44 -0300

    #update in cats.controller use decorator @UseInterceptors(LggingInterceptor) to measure all call method execution. If this decorator was puted in a only method, this would go behave only for it.

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, SetMetadata, UseInterceptors } from '@nestjs/common';
 import { Roles } from 'src/common/guards/roles/roles.decorator';
+import { LoggingInterceptor } from 'src/common/interceptors/logging.interceptor';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 @Controller('cats')
+@UseInterceptors(LoggingInterceptor)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
