479814ecd26feb3b730361bc138c3eda626ec135
halysson <halyssonfreitas@gmail.com>
2022-01-20 17:50:57 -0300

    #update user.entity.ts to have typeorm decorators

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
