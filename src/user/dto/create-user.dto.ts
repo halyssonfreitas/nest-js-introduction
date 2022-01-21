f6ad1f972fe1f8401c69150d476f671674a1eca3
halysson <halyssonfreitas@gmail.com>
2022-01-21 09:49:48 -0300

    #update create-user.dto.ts and #test this with a POST request by Insomnia. The incomplete JSON return a error JSON like was expected

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsBoolean, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    firstName: string;
+    @IsString()
+    lastName: string;
+    @IsBoolean()
+    isActive: boolean;
+}
