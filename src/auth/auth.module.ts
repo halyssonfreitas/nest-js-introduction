e9d09c73424278482f56a7a75c1b9654b582bbe6
halysson <halyssonfreitas@gmail.com>
2022-01-22 17:12:37 -0300

    #update auth.module to import JwtModule and register it

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local/local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1m' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
